415b9aed4912453e1b972f51b9e99b7328def190
Yurii Baranchuk <Yurii.Baranchuk@infopulse.com>
2024-03-20 10:47:49 +0200

    added VM to main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,28 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = random_pet.rg_name.id
 }
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                            = "${var.prefix}-vm"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  size                            = var.vm_size
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "resource_group_location" {
   type        = string
-  default     = "eastus"
+  default     = "eastus2"
   description = "Location of the resource group."
 }
 
@@ -8,4 +8,31 @@ variable "resource_group_name_prefix" {
   type        = string
   default     = "rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  type        = string
+  default     = "eastus2"
+  description = "The Azure Region in which all resources in this example should be created."
+}
+
+variable "admin_username" {
+  type        = string
+  default     = "admin"
+  description = "The admin username for the VM being created."
+}
+
+variable "admin_password" {
+  type        = string
+  default     = "admin228!"
+  description = "The password for the VM being created."
+}
+
+variable "vm_size" {
+  description = "Specifies the size of the virtual machine."
+  default     = "Standard_B1s"
 }
